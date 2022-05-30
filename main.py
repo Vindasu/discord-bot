e1633d4adb90bb2b9cb049ba216bd153b53a049b
Alex <alexleeadams@aol.com>
2022-05-30 12:09:11 -0500

    it works!

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-import discord
+# bot.py
 import os
+
+import discord
+
+TOKEN = "OTgwMzM5NTYxOTAzMzA0NzI0.GtmUW_.3d9nMAurhZjQh832noAqEWLpuZJ8ZZbP2pWBIQ"
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("{0.user} is online!".format(client))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    elif message.content.startswith("!hello") or message.content.startswith("/hello"):
+        await message.channel.send("Hello from the other side!")
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
 
-client.run(os.getenv('TOKEN'))
+client.run(TOKEN)
